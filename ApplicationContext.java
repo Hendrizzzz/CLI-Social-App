8316c9b77961b0954fc34ebbcb1b28f21c2ec3de
JIM HENDRIX BAG-EO <2243493@slu.edu.ph>
2024-10-11 19:35:40 +0800

    Initial Commit

---
+++ ApplicationContext.java
@@ -0,0 +1,9 @@
+import java.util.Optional;
+
+/**
+ * The class to handle declaration and instantiation of core classes. Plus dependency injections.
+ */
+public class ApplicationContext {
+  
+}
+
---
+++ Main.java
@@ -0,0 +1,5 @@
+/**
+ * The class to start the application
+ */
+public class Main {
+}
